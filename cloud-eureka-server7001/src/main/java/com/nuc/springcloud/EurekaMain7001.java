80f956f05b5a8a59e951489d7d7b1b36852f12fa
iniwu <mobiwusihuan288@163.com>
2023-07-19 11:09:28 +0800

    eureka添加安全访问控制

--- cloud-eureka-server7001/src/main/java/com/nuc/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/nuc/springcloud/EurekaMain7001.java
@@ -3,6 +3,9 @@ package com.nuc.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author mobiw
@@ -14,5 +17,18 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaMain7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7001.class,args);
+    }
+
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // 方法1：关闭csrf
+            http.csrf().disable();
+            // 方法2：忽略/eureka/** 所有请求
+            // http.csrf().ignoringAntMatchers("/eureka/**");
+            // 访问eureka控制台和actuator控制台时能做安全控制
+            super.configure(http);
+        }
     }
 }
